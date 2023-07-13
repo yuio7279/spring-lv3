b7d25d0238de187d2f338827fbb480713b91f508
yuio7279 <yuio7279@gmail.com>
2023-07-14 03:57:12 +0900

    댓글 entity 작성

---
+++ src/main/java/com/sparta/post/entity/Comment.java
@@ -0,0 +1,35 @@
+package com.sparta.post.entity;
+
+import com.sparta.post.dto.CommentRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comment")
+@NoArgsConstructor
+public class Comment extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    public Comment(User user, Post post, CommentRequestDto requestDto) {
+        this.user = user;
+        this.post = post;
+        this.content = requestDto.getContent();
+    }
+}
