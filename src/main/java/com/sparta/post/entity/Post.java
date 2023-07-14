685bd991ea23e7a5c9ad64994a643093553c1531
yuio7279 <yuio7279@gmail.com>
2023-07-14 10:05:38 +0900

    게시물에 댓글 조회 완료 "CommentResponseDto를 사용하기"

--- src/main/java/com/sparta/post/entity/Post.java
+++ src/main/java/com/sparta/post/entity/Post.java
@@ -4,7 +4,6 @@ import com.sparta.post.dto.PostRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
